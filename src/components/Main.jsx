e3265551ce48dc2fe1f967ca585344aab24bd567
SAMURAY-A <abdullohaziz559@gmail.com>
2024-12-09 12:54:32 +0500

    salom

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -80,7 +80,7 @@ const Main = () => {
     <div style={{ padding: "20px" }}>
         <div className="flex justify-center">
         <h1 className="inline-block px-4 py-2 text-white text-2xl font-[Parkinsans] bg-blue-600 rounded">
-          News Management
+          News
         </h1>
         </div>
       <div className="flex justify-end">
